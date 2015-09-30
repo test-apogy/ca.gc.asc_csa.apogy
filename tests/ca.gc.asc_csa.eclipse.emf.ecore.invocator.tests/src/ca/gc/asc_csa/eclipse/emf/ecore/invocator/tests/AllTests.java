fe02126693852730b6902e369e88b11b2dbe69d2
Regent L'Archeveque <regent.larcheveque@asc-csa.gc.ca>
2015-09-30 15:42:47 -0400

    Disable invocator.tests.AllTests in pom.xml.

--- tests/ca.gc.asc_csa.eclipse.emf.ecore.invocator.tests/src/ca/gc/asc_csa/eclipse/emf/ecore/invocator/tests/AllTests.java
+++ tests/ca.gc.asc_csa.eclipse.emf.ecore.invocator.tests/src/ca/gc/asc_csa/eclipse/emf/ecore/invocator/tests/AllTests.java
@@ -2,8 +2,9 @@ package ca.gc.asc_csa.eclipse.emf.ecore.invocator.tests;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-//@SuiteClasses({ AbstractResultToEReferenceValueToImageSnapshotToAbstractEImageConverterTest.class })
+@SuiteClasses({ AbstractResultToEReferenceValueToImageSnapshotToAbstractEImageConverterTest.class })
 public class AllTests {
 }
