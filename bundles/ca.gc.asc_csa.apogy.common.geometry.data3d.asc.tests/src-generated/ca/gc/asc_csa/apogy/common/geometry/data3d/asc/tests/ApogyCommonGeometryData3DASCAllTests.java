f733d8aec441a5c4d2f36979e4d07a03a0fe33e8
Sebastien Gemme <sebastien.gemme@asc-csa.gc.ca>
2016-01-29 08:14:28 -0500

    Renaming
    Signed-off-by: Sebastien Gemme <sebastien.gemme@asc-csa.gc.ca>

---
+++ bundles/ca.gc.asc_csa.apogy.common.geometry.data3d.asc.tests/src-generated/ca/gc/asc_csa/apogy/common/geometry/data3d/asc/tests/ApogyCommonGeometryData3DASCAllTests.java
@@ -0,0 +1,48 @@
+/**
+ * Agence spatiale canadienne / Canadian Space Agency 2015 Copyrights (c)
+ */
+package ca.gc.asc_csa.apogy.common.geometry.data3d.asc.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>ApogyCommonGeometryData3DASC</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ApogyCommonGeometryData3DASCAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new ApogyCommonGeometryData3DASCAllTests("ApogyCommonGeometryData3DASC Tests");
+		suite.addTest(ApogyCommonGeometryData3DASCTests.suite());
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ApogyCommonGeometryData3DASCAllTests(String name) {
+		super(name);
+	}
+
+} //ApogyCommonGeometryData3DASCAllTests
