7c66bf0fefdcbf3dd7178a961053b36fc1cd1b16
Sebastien Gemme <sebastien.gemme@canada.ca>
2016-02-01 09:44:57 -0500

    apply_comments.sh also supports xcore files
    Signed-off-by: Sebastien Gemme <sebastien.gemme@canada.ca>

--- admin/scripts/copyrights/apply_comments.sh
+++ admin/scripts/copyrights/apply_comments.sh
@@ -6,7 +6,7 @@ COPYRIGHT=$(dirname $0)/../../org.eclipse.symphony.admin/legal/copyrights.txt
 echo "CSCRIPT: $CSCRIPT"
 
 # First strip the comments
-for i in $(find . -name \*.java -o -name \*.MF -o -name \*.xml)
+for i in $(find . -name \*.java -o -name \*.MF -o -name \*.xml -o -name \*.xcore)
 do
     tmpFileName=$(tempfile)
     $CSCRIPT strip $i > $tmpFileName
