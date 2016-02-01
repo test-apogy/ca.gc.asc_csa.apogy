23b7ea5c12a2bbe11281281e4cbf857b2a09778d
Sebastien Gemme <sebastien.gemme@canada.ca>
2016-02-01 09:04:40 -0500

    Renamed apply comment script
    Signed-off-by: Sebastien Gemme <sebastien.gemme@canada.ca>

---
+++ admin/scripts/copyrights/apply_comments.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CSCRIPT=$(dirname $0)/copyright-adder.py
+COPYRIGHT=$(dirname $0)/../../org.eclipse.symphony.admin/legal/copyrights.txt
+
+echo "CSCRIPT: $CSCRIPT"
+
+# First strip the comments
+for i in $(find . -name \*.java -o -name \*.MF -o -name \*.xml)
+do
+    tmpFileName=$(tempfile)
+    $CSCRIPT strip $i > $tmpFileName
+    mv $tmpFileName $i
+
+    $CSCRIPT apply $i $COPYRIGHT > $tmpFileName
+    mv $tmpFile $i
+done
+
