4c138ec73901d2edfa30b7b1cdd6730fd67cc6f2
Sebastien Gemme <sebastien.gemme@canada.ca>
2016-02-04 10:36:17 -0500

    Added comments to properties file
    Signed-off-by: Sebastien Gemme <sebastien.gemme@canada.ca>

--- admin/scripts/copyrights/apply_comments.sh
+++ admin/scripts/copyrights/apply_comments.sh
@@ -6,7 +6,8 @@ COPYRIGHT=$(dirname $0)/../../org.eclipse.symphony.admin/legal/copyrights.txt
 echo "CSCRIPT: $CSCRIPT"
 
 # First strip the comments
-for i in $(find . -name \*.java -o -name \*.xml -o -name \*.xcore ! -type d)
+#for i in $(find . -name \*.java -o -name \*.xml -o -name \*.xcore ! -type d)
+for i in $(find . -name \*.properties )
 do
     tmpFileName=$(tempfile)
     $CSCRIPT strip $i > $tmpFileName
