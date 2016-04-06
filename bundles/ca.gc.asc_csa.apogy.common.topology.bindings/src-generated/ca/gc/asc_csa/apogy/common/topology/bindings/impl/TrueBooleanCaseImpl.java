467025afbcf00d2afdfd27a620d2df612aec614f
Pierre Allard <pierre.allard@canada.ca>
2016-04-06 14:02:25 -0400

    Fixes.

---
+++ bundles/ca.gc.asc_csa.apogy.common.topology.bindings/src-generated/ca/gc/asc_csa/apogy/common/topology/bindings/impl/TrueBooleanCaseImpl.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2015-2016 Canadian Space Agency (CSA) / Agence spatiale canadienne (ASC).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Pierre Allard (Pierre.Allard@canada.ca), 
+ *     Regent L'Archeveque (Regent.Larcheveque@canada.ca),
+ *     Sebastien Gemme (Sebastien.Gemme@canada.ca),
+ *     Canadian Space Agency (CSA) - Initial API and implementation
+ */
+package ca.gc.asc_csa.apogy.common.topology.bindings.impl;
+
+import ca.gc.asc_csa.apogy.common.topology.bindings.ApogyCommonTopologyBindingsPackage;
+import ca.gc.asc_csa.apogy.common.topology.bindings.TrueBooleanCase;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>True Boolean Case</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class TrueBooleanCaseImpl extends BooleanCaseImpl implements TrueBooleanCase {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected TrueBooleanCaseImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ApogyCommonTopologyBindingsPackage.Literals.TRUE_BOOLEAN_CASE;
+	}
+
+} //TrueBooleanCaseImpl
