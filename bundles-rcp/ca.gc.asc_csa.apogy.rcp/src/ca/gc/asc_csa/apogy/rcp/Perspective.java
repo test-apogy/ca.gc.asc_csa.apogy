40e8deee4eefd9b4b4cd391bbd802f04d5e47720
Regent L'Archeveque <regent.larcheveque@canada.ca>
2016-05-20 10:35:30 -0400

    Problems with legacy e3 views.

---
+++ bundles-rcp/ca.gc.asc_csa.apogy.rcp/src/ca/gc/asc_csa/apogy/rcp/Perspective.java
@@ -0,0 +1,12 @@
+package ca.gc.asc_csa.apogy.rcp;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public static final String ID = "ca.gc.asc_csa.apogy.rcp.perspective";
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
