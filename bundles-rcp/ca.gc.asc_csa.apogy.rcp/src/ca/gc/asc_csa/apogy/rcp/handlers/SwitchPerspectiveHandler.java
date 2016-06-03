7867eb6c4c14b94b49836eb8130fc3d5424d40bb
Regent L'Archeveque <regent.larcheveque@canada.ca>
2016-06-03 13:51:49 -0400

    Perspective switcher works.

--- bundles-rcp/ca.gc.asc_csa.apogy.rcp/src/ca/gc/asc_csa/apogy/rcp/handlers/SwitchPerspectiveHandler.java
+++ bundles-rcp/ca.gc.asc_csa.apogy.rcp/src/ca/gc/asc_csa/apogy/rcp/handlers/SwitchPerspectiveHandler.java
@@ -11,11 +11,13 @@
  *******************************************************************************/
 package ca.gc.asc_csa.apogy.rcp.handlers;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
@@ -23,18 +25,38 @@ import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
 public class SwitchPerspectiveHandler {
 	@Execute
-	public void execute(MWindow window, EPartService partService, EModelService modelService, @Named("ca.gc.asc_csa.apogy.rcp.commands.switchPerspectiveCommandPerspectiveId") String id) {
+	public void execute(MPerspective activePerspective, 
+			            MApplication app,
+			            EPartService partService, 
+			            EModelService modelService, 
+			            @Named("ca.gc.asc_csa.apogy.rcp.commands.switchPerspectiveCommandPerspectiveId") String id, 
+			            @Named("ca.gc.asc_csa.apogy.rcp.commands.switchPerspectiveCommandPerspectiveLabel") String label){
 		System.out.println("SwitchPerspectiveHandler.execute(): " + id);
+		System.out.println("SwitchPerspectiveHandler.execute(): active = " + activePerspective.getElementId());
 		
-//		MPerspective element = (MPerspective) modelService.find(id, app);
-	    List<MPerspective> perspectives = modelService.findElements(window,
-	            id, MPerspective.class, null);		
+		List<MPerspective> perspectives = modelService.findElements(app, id, MPerspective.class, null);
+
+		
+		System.out.println("SwitchPerspectiveHandler.execute(): Registered Perspectives:");
+		for (Iterator iterator = perspectives.iterator(); iterator.hasNext();) {
+			MPerspective mPerspective = (MPerspective) iterator.next();			
+			System.out.println(mPerspective.getElementId());			
+		}
+		System.out.println("---------------------------------------------------------------");
+		
+		
+		MPerspective element = (MPerspective) modelService.find(id, app);	
+		if (element == null){
+			element = (MPerspective) modelService.find(id + "<" + label + ">", app);
+		}
 		
 		// now switch perspective
-		if (!perspectives.isEmpty()){
-			partService.switchPerspective(perspectives.get(0));
+		if (element!=null){
+			if (element != activePerspective){
+				partService.switchPerspective(element);
+			}
 		}else{			
-			throw new RuntimeException("Perspective <"+ id + "> not found.");
+			System.out.println("SwitchPerspectiveHandler.execute() Perspective <"+ id + "> not found.");
 		}
 	}
 }
