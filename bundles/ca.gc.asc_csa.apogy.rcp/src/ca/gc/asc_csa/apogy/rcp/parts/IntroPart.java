b07f9e42b751453976bae38889172cf13480e5e0
Regent L'Archeveque <regent.larcheveque@canada.ca>
2016-10-17 11:01:57 -0400

    Converted apogy.doc into Java project.

--- bundles/ca.gc.asc_csa.apogy.rcp/src/ca/gc/asc_csa/apogy/rcp/parts/IntroPart.java
+++ bundles/ca.gc.asc_csa.apogy.rcp/src/ca/gc/asc_csa/apogy/rcp/parts/IntroPart.java
@@ -4,30 +4,24 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.eclipse.e4.ui.di.Focus;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.forms.widgets.Hyperlink;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.events.IHyperlinkListener;
 import org.eclipse.ui.forms.widgets.FormText;
+import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ImageHyperlink;
-import org.eclipse.wb.swt.ResourceManager;
-import org.eclipse.ui.forms.widgets.ColumnLayout;
-import org.eclipse.ui.forms.widgets.ColumnLayoutData;
 import org.eclipse.ui.forms.widgets.Section;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.custom.StyledText;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+import ca.gc.asc_csa.apogy.doc.Constants;
 
 public class IntroPart {
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
-
-	public IntroPart() {
-	}
 
 	/**
 	 * Create contents of the view part.
@@ -38,18 +32,18 @@ public class IntroPart {
 		gl_parent.verticalSpacing = 10;
 		parent.setLayout(gl_parent);
 		
-		Section sctnNewSection = formToolkit.createSection(parent, Section.TITLE_BAR);
-		sctnNewSection.setForeground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
-		sctnNewSection.setFont(SWTResourceManager.getFont("Ubuntu", 25, SWT.BOLD));
-		GridData gd_sctnNewSection = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1);
-		gd_sctnNewSection.widthHint = 278;
-		sctnNewSection.setLayoutData(gd_sctnNewSection);
-		formToolkit.paintBordersFor(sctnNewSection);
-		sctnNewSection.setText("Apogy");
+		Section sctnNewSectionDescription = formToolkit.createSection(parent, Section.TITLE_BAR);
+		sctnNewSectionDescription.setForeground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
+		sctnNewSectionDescription.setFont(SWTResourceManager.getFont("Ubuntu", 25, SWT.BOLD));
+		GridData gd_sctnNewSectionDescription = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1);
+		gd_sctnNewSectionDescription.widthHint = 278;
+		sctnNewSectionDescription.setLayoutData(gd_sctnNewSectionDescription);
+		formToolkit.paintBordersFor(sctnNewSectionDescription);
+		sctnNewSectionDescription.setText("Apogy");
 		
-		Composite composite = formToolkit.createComposite(sctnNewSection, SWT.NONE);
+		Composite composite = formToolkit.createComposite(sctnNewSectionDescription, SWT.NONE);
 		formToolkit.paintBordersFor(composite);
-		sctnNewSection.setClient(composite);
+		sctnNewSectionDescription.setClient(composite);
 		composite.setLayout(new GridLayout(1, false));
 		
 		FormText formText = formToolkit.createFormText(composite, false);
@@ -61,53 +55,94 @@ public class IntroPart {
 		formToolkit.paintBordersFor(formText);
 		formText.setText("Apogy is a multi-mission operations and planning software framework. Apogy uses Eclipse Modeling Framework models to define the systems being controlled, the environment in which these systems are operated, the operations being carried out and the corresponding data produced during operations.", false, false);
 		
-		Section sctnNewSection_1 = formToolkit.createSection(parent, Section.TITLE_BAR);
-		sctnNewSection_1.setForeground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
-		sctnNewSection_1.setFont(SWTResourceManager.getFont("Ubuntu", 20, SWT.BOLD));
-		sctnNewSection_1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1));
-		formToolkit.paintBordersFor(sctnNewSection_1);
-		sctnNewSection_1.setText("What would you like to do?");
-		sctnNewSection_1.setExpanded(true);
+		Section sctnNewSectionTodo = formToolkit.createSection(parent, Section.TITLE_BAR);
+		sctnNewSectionTodo.setForeground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
+		sctnNewSectionTodo.setFont(SWTResourceManager.getFont("Ubuntu", 20, SWT.BOLD));
+		sctnNewSectionTodo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1));
+		formToolkit.paintBordersFor(sctnNewSectionTodo);
+		sctnNewSectionTodo.setText("What would you like to do?");
+		sctnNewSectionTodo.setExpanded(true);
 		
-		Composite composite_1 = formToolkit.createComposite(sctnNewSection_1, SWT.NONE);
+		Composite composite_1 = formToolkit.createComposite(sctnNewSectionTodo, SWT.NONE);
 		formToolkit.paintBordersFor(composite_1);
-		sctnNewSection_1.setClient(composite_1);
+		sctnNewSectionTodo.setClient(composite_1);
 		GridLayout gl_composite_1 = new GridLayout(1, false);
 		gl_composite_1.verticalSpacing = 20;
 		composite_1.setLayout(gl_composite_1);
 		
-		Label lblNewLabel = formToolkit.createLabel(composite_1, "", SWT.NONE);
+		formToolkit.createLabel(composite_1, "", SWT.NONE);
 		
-		ImageHyperlink mghprlnkNewImagehyperlink_3 = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
-		mghprlnkNewImagehyperlink_3.setToolTipText("Create a new Apogy project.");
-		mghprlnkNewImagehyperlink_3.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
-		formToolkit.paintBordersFor(mghprlnkNewImagehyperlink_3);
-		mghprlnkNewImagehyperlink_3.setText("New Project");
+		ImageHyperlink newProjectHyperlink = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
+		newProjectHyperlink.addHyperlinkListener(new IHyperlinkListener() {
+			public void linkActivated(HyperlinkEvent e) {
+			}
+			public void linkEntered(HyperlinkEvent e) {
+			}
+			public void linkExited(HyperlinkEvent e) {
+			}
+		});
+		newProjectHyperlink.setToolTipText("Create a new Apogy project.");
+		newProjectHyperlink.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
+		formToolkit.paintBordersFor(newProjectHyperlink);
+		newProjectHyperlink.setText("New Project");
 		
-		ImageHyperlink mghprlnkNewImagehyperlink_2 = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
-		mghprlnkNewImagehyperlink_2.setToolTipText("Open an existing Apogy project.");
-		mghprlnkNewImagehyperlink_2.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
-		formToolkit.paintBordersFor(mghprlnkNewImagehyperlink_2);
-		mghprlnkNewImagehyperlink_2.setText("Open Project");
+		ImageHyperlink openProjectHyperlink = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
+		openProjectHyperlink.addHyperlinkListener(new IHyperlinkListener() {
+			public void linkActivated(HyperlinkEvent e) {
+			}
+			public void linkEntered(HyperlinkEvent e) {
+			}
+			public void linkExited(HyperlinkEvent e) {
+			}
+		});
+		openProjectHyperlink.setToolTipText("Open an existing Apogy project.");
+		openProjectHyperlink.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
+		formToolkit.paintBordersFor(openProjectHyperlink);
+		openProjectHyperlink.setText("Open Project");
 		
-		ImageHyperlink mghprlnkNewImagehyperlink = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
-		mghprlnkNewImagehyperlink.setToolTipText("Open an Apogy project example.");
-		mghprlnkNewImagehyperlink.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
-		mghprlnkNewImagehyperlink.setImage(null);
-		formToolkit.paintBordersFor(mghprlnkNewImagehyperlink);
-		mghprlnkNewImagehyperlink.setText("Examples");
+		ImageHyperlink examplesHyperlink = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
+		examplesHyperlink.addHyperlinkListener(new IHyperlinkListener() {
+			public void linkActivated(HyperlinkEvent e) {
+			}
+			public void linkEntered(HyperlinkEvent e) {
+			}
+			public void linkExited(HyperlinkEvent e) {
+			}
+		});
+		examplesHyperlink.setToolTipText("Open an Apogy project example.");
+		examplesHyperlink.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
+		examplesHyperlink.setImage(null);
+		formToolkit.paintBordersFor(examplesHyperlink);
+		examplesHyperlink.setText("Examples");
 		
-		ImageHyperlink mghprlnkNewImagehyperlink_1 = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
-		mghprlnkNewImagehyperlink_1.setToolTipText("Jump into the available Apogy tutorials.");
-		mghprlnkNewImagehyperlink_1.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
-		formToolkit.paintBordersFor(mghprlnkNewImagehyperlink_1);
-		mghprlnkNewImagehyperlink_1.setText("Tutorials");
+		ImageHyperlink tutorialsHyperlink = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
+		tutorialsHyperlink.addHyperlinkListener(new IHyperlinkListener() {
+			public void linkActivated(HyperlinkEvent e) {
+			}
+			public void linkEntered(HyperlinkEvent e) {
+			}
+			public void linkExited(HyperlinkEvent e) {
+			}
+		});
+		tutorialsHyperlink.setToolTipText("Jump into the available Apogy tutorials.");
+		tutorialsHyperlink.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
+		formToolkit.paintBordersFor(tutorialsHyperlink);
+		tutorialsHyperlink.setText("Tutorials");
 		
-		ImageHyperlink mghprlnkHelp = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
-		mghprlnkHelp.setToolTipText("Browse the Apogy documentation.");
-		mghprlnkHelp.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
-		formToolkit.paintBordersFor(mghprlnkHelp);
-		mghprlnkHelp.setText("Help");
+		ImageHyperlink helpHyperLink = formToolkit.createImageHyperlink(composite_1, SWT.NONE);
+		helpHyperLink.addHyperlinkListener(new IHyperlinkListener() {
+			public void linkActivated(HyperlinkEvent e) {
+				PlatformUI.getWorkbench().getHelpSystem().displayHelpResource(Constants.APOGY_DOC_TOC_LOCATION);
+			}
+			public void linkEntered(HyperlinkEvent e) {
+			}
+			public void linkExited(HyperlinkEvent e) {
+			}
+		});
+		helpHyperLink.setToolTipText("Browse the Apogy documentation.");
+		helpHyperLink.setFont(SWTResourceManager.getFont("Ubuntu", 15, SWT.NORMAL));
+		formToolkit.paintBordersFor(helpHyperLink);
+		helpHyperLink.setText("Help");
 	}
 
 	@PreDestroy
@@ -116,6 +151,5 @@ public class IntroPart {
 
 	@Focus
 	public void setFocus() {
-		// TODO	Set the focus to control
 	}
 }
---
+++ doc/ca.gc.asc_csa.apogy.doc/src/ca/gc/asc_csa/apogy/doc/Constants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Canadian Space Agency (CSA) / Agence spatiale canadienne (ASC).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Pierre Allard (Pierre.Allard@canada.ca), 
+ *     Regent L'Archeveque (Regent.Larcheveque@canada.ca),
+ *     Sebastien Gemme (Sebastien.Gemme@canada.ca),
+ *     Canadian Space Agency (CSA) - Initial API and implementation
+ */
+package ca.gc.asc_csa.apogy.doc;
+
+/*
+ * The following class contains Apogy Documentation Constants.
+ */
+public class Constants {
+	private Constants(){		
+	}
+	
+	public static final String APOGY_DOC_TOC_LOCATION = "/" + Activator.ID + "wiki/toc.xml"; 
+	public static final String APOGY_DOC_TUTORIALS_LOCATION = "/" + Activator.ID + "wiki/tutorials.html";
+}
