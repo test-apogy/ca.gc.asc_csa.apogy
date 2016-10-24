24eb97ae69847af9bcd711f91cdafa2dde5c16e4
Olivier L. Larouche <olivier.llarouche@canada.ca>
2016-10-24 11:34:37 -0400

    Working on new button.

--- bundles/ca.gc.asc_csa.apogy.core.invocator.ui/src/ca/gc/asc_csa/apogy/core/invocator/ui/composites/AdvancedEditorComposite.java
+++ bundles/ca.gc.asc_csa.apogy.core.invocator.ui/src/ca/gc/asc_csa/apogy/core/invocator/ui/composites/AdvancedEditorComposite.java
@@ -14,7 +14,19 @@ package ca.gc.asc_csa.apogy.core.invocator.ui.composites;
  *     Canadian Space Agency (CSA) - Initial API and implementation
  */
 
+import java.util.List;
+
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.PojoProperties;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
@@ -29,87 +41,151 @@ import org.eclipse.swt.widgets.Listener;
 
 import ca.gc.asc_csa.apogy.common.emf.ui.composites.EObjectComposite;
 import ca.gc.asc_csa.apogy.core.invocator.ApogyCoreInvocatorFacade;
+import ca.gc.asc_csa.apogy.core.invocator.ApogyCoreInvocatorFactory;
+import ca.gc.asc_csa.apogy.core.invocator.InvocatorSession;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
 
-public class AdvancedEditorComposite extends Composite implements ISelectionProvider{
-	
+public class AdvancedEditorComposite extends Composite implements ISelectionProvider {
+
 	private ISelectionChangedListener selectionChangedListener;
 	private EObjectComposite eObjectComposite;
+	private DataBindingContext bindingContext;
 	
+	private Button btnNew;
 	
+	private boolean eObjectNotFull;
+
 	/**
 	 * Creates the composite.
 	 * 
 	 * @param parent
 	 * @param style
 	 */
-	public AdvancedEditorComposite(Composite parent, int style, ESelectionService selectionService) {
+	public AdvancedEditorComposite(Composite parent, int style, ESelectionService selectionService, EObject eObject) {
 		super(parent, style);
+		System.out.println("AdvancedEditorComposite.AdvancedEditorComposite()");
 
 		addDisposeListener(new DisposeListener() {
 			public void widgetDisposed(DisposeEvent e) {
 				dispose();
 			}
 		});
+		setLayout(new GridLayout(2, false));
 
-		eObjectComposite = new EObjectComposite(this,
-				SWT.None) {
-			@Override
-			protected void newSelection(ISelection selection) {
-				selectionService.setSelection(selection);
-			}
+		eObjectComposite = new EObjectComposite(this, SWT.None) {
+			// @Override
+			// protected void newSelection(ISelection selection) {
+			// selectionService.setSelection(selection);
+			// }
 		};
-	
-		if (ApogyCoreInvocatorFacade.INSTANCE.getActiveInvocatorSession() != null){
-			eObjectComposite.setEObject(ApogyCoreInvocatorFacade.INSTANCE.getActiveInvocatorSession());
-		}
+		eObjectComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 2));
+		//
+		// if(eObject != null){
+		// eObjectComposite.setEObject(eObject);
+		// }else{
+		eObjectComposite.setEObject(eObject);
+		// }
 		
-		Button btnNewChild = new Button(this, SWT.NONE);
-		btnNewChild.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
-		btnNewChild.setText("New child");
-		btnNewChild.addListener(SWT.Selection, new Listener() {
+		eObjectNotFull = !isEObjectFull(eObjectComposite.getSelectedEObject());
+	
+		
+
+		btnNew = new Button(this, SWT.NONE);
+		btnNew.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		btnNew.setText("New");
+		btnNew.setEnabled(true);
+		btnNew.addListener(SWT.Selection, new Listener() {
 			@Override
 			public void handleEvent(Event event) {
-				if(event.type == SWT.Selection){
-					// TODO: New Child button clicked
+				if (event.type == SWT.Selection) {
+
 				}
-				
+				// selectedEClass.getEAllStructuralFeatures();
+				// TODO: New Child button clicked
 			}
 		});
-		
+
 		Button btnDelete = new Button(this, SWT.NONE);
-		btnDelete.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		btnDelete.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false, 1, 1));
 		btnDelete.setText("Delete");
 		btnDelete.addListener(SWT.Selection, new Listener() {
 			@Override
 			public void handleEvent(Event event) {
-				if(event.type == SWT.Selection){
+				if (event.type == SWT.Selection) {
 					// TODO: Delete button clicked
 				}
-				
+
 			}
 		});
 		
-		Button btnValidate = new Button(this, SWT.NONE);
-		btnValidate.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false, 1, 1));
-		btnValidate.setText("Validate");
-		btnValidate.addListener(SWT.Selection, new Listener() {
-			@Override
-			public void handleEvent(Event event) {
-				if(event.type == SWT.Selection){
-					// TODO: Validate button clicked
-				}
-				
-			}
-		});
+		bindingContext = initDataBindings();
 	}
-	
+
 	@Override
 	public void dispose() {
 		super.dispose();
-		if(selectionChangedListener != null){
+		if (selectionChangedListener != null) {
 			eObjectComposite.removeListener(SWT.Selection, (Listener) selectionChangedListener);
 		}
 	}
+
+	@SuppressWarnings("unchecked")
+	private DataBindingContext initDataBindings(){
+		DataBindingContext m_bindingContext = new DataBindingContext();
+		
+		IObservableValue<?> observeButtonNewEnabled = WidgetProperties.enabled().observe(btnNew);
+		IObservableValue<?> observeEObjectFull = PojoProperties.value("EObjectFull", this.getClass()).observe(this);
+		
+		m_bindingContext.bindValue(observeButtonNewEnabled, observeEObjectFull);
+		
+		return m_bindingContext;
+	}
+	
+	private boolean isEObjectFull(EObject eObject){
+		boolean eObjectFull = true;
+		System.out.println("AdvancedEditorComposite.isEObjectFull()");
+		if (eObject != null) {
+			//EClass selectedEClass = eObject.eClass();
+			System.out.println("AdvancedEditorComposite.EObjectFull() : " + eObjectFull);
+			System.out.println("AdvancedEditorComposite.EObjectFull() : " + eObject);
+			System.out.println("AdvancedEditorComposite.EObjectFull() : " + eObject.eClass().getEAllStructuralFeatures());
+			for (EStructuralFeature structuralFeature : eObject.eClass().getEAllStructuralFeatures()) {
+				//EReference eref = structuralFeature.get;
+				//            		EReference)referenceEObject;
+				final Object value = eObject.eGet(structuralFeature);
+				System.out.println("AdvancedEditorComposite.isEObjectFull() : value : " + value);
+				if (value == null) {
+					eObjectFull = false;
+					break;
+				}
+				if (value instanceof List) {
+					eObjectFull = false;
+					break;
+				}
+			}
+		}else{
+			return true;
+		}
+		//		//EList<EStructuralFeature> structuralFeatures = selectedEClass.getEAllStructuralFeatures();
+//		EList<EAttribute> attributes = selectedEClass.getEAllAttributes();
+//		//eObject.is
+//		for(EStructuralFeature structuralFeatures: selectedEClass.getEAllStructuralFeatures()){
+//			if(structuralFeatures. && eObject.eGet(structuralFeatures) != null){
+//				eObjectFull = false;
+//			}else{
+//				if(eObject.eGet(structuralFeatures))
+//			}
+//		}
+		System.out.println("AdvancedEditorComposite.isEObjectFull( : END : )" + eObjectFull);
+		return eObjectFull ;
+	}
+	
+	public void setEObject(EObject eObject){
+		eObjectComposite.setEObject(eObject);
+		eObjectNotFull = !isEObjectFull(eObject);
+	}
+	
 	
 	/**
 	 * Selection provider methods
@@ -117,7 +193,7 @@ public class AdvancedEditorComposite extends Composite implements ISelectionProv
 	@Override
 	public void addSelectionChangedListener(ISelectionChangedListener listener) {
 		selectionChangedListener = listener;
-		eObjectComposite.addListener(SWT.Selection, (Listener) listener); 
+		eObjectComposite.addListener(SWT.Selection, (Listener) listener);
 	}
 
 	@Override
@@ -133,5 +209,4 @@ public class AdvancedEditorComposite extends Composite implements ISelectionProv
 	@Override
 	public void setSelection(ISelection selection) {
 	}
-	
 }
--- bundles/ca.gc.asc_csa.apogy.core.invocator.ui/src/ca/gc/asc_csa/apogy/core/invocator/ui/parts/ApogyAdvancedEditorPart.java
+++ bundles/ca.gc.asc_csa.apogy.core.invocator.ui/src/ca/gc/asc_csa/apogy/core/invocator/ui/parts/ApogyAdvancedEditorPart.java
@@ -16,21 +16,38 @@ package ca.gc.asc_csa.apogy.core.invocator.ui.parts;
 import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 import ca.gc.asc_csa.apogy.common.emf.ui.composites.EObjectComposite;
 import ca.gc.asc_csa.apogy.core.invocator.ApogyCoreInvocatorFacade;
+import ca.gc.asc_csa.apogy.core.invocator.ApogyCoreInvocatorFactory;
+import ca.gc.asc_csa.apogy.core.invocator.ApogyCoreInvocatorPackage;
+import ca.gc.asc_csa.apogy.core.invocator.InvocatorSession;
 import ca.gc.asc_csa.apogy.core.invocator.ui.composites.AdvancedEditorComposite;
 
 public class ApogyAdvancedEditorPart{
 
 //	@Inject ESelectionService selectionService;
+	private AdvancedEditorComposite advancedEditorComposite;
+	private Adapter adapter;
 	
 	@PostConstruct	
 	public void createPartControl(Composite parent, ESelectionService selectionService) {
-		AdvancedEditorComposite advancedEditorComposite = new AdvancedEditorComposite(parent, SWT.None, selectionService);
+		advancedEditorComposite = new AdvancedEditorComposite(parent, SWT.None, selectionService, null);
+		InvocatorSession activeSession = ApogyCoreInvocatorFacade.INSTANCE.getActiveInvocatorSession();
+		advancedEditorComposite.setEObject(activeSession);
+		
+		ApogyCoreInvocatorFacade.INSTANCE.eAdapters().add(getApogyCoreInvocatorFacadeAdapter());
+//		EObjectComposite eObjectComposite = new EObjectComposite(parent, SWT.None);
+//		eObjectComposite.setEObject(ApogyCoreInvocatorFactory.eINSTANCE.createInvocatorSession());
 //		EObjectComposite eObjectComposite = new EObjectComposite(parent,
 //				SWT.None) {
 //			@Override
@@ -43,4 +60,23 @@ public class ApogyAdvancedEditorPart{
 //			eObjectComposite.setEObject(ApogyCoreInvocatorFacade.INSTANCE.getActiveInvocatorSession());
 //		}
 	}
+	
+
+
+public void updateSelection(ISelection arg0) {
+	advancedEditorComposite.setEObject((EObject)arg0);
+}		
+
+
+private Adapter getApogyCoreInvocatorFacadeAdapter() {
+	if (adapter == null) {
+		adapter = new AdapterImpl() {
+			@Override
+			public void notifyChanged(Notification msg) {
+				advancedEditorComposite.setEObject((EObject) msg.getFeature());
+			}
+		};
+	}
+	return adapter;
+}
 }
