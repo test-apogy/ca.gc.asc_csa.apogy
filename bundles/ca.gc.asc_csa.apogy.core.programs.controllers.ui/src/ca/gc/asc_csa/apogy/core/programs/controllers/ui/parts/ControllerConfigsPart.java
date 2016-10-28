faf764da76e295b78f93e354afb3fe2870e9bb6b
Olivier L. Larouche <olivier.llarouche@canada.ca>
2016-10-28 14:05:01 -0400

    Added Named Described list composite

--- bundles/ca.gc.asc_csa.apogy.core.programs.controllers.ui/src/ca/gc/asc_csa/apogy/core/programs/controllers/ui/parts/ControllerConfigsPart.java
+++ bundles/ca.gc.asc_csa.apogy.core.programs.controllers.ui/src/ca/gc/asc_csa/apogy/core/programs/controllers/ui/parts/ControllerConfigsPart.java
@@ -13,27 +13,142 @@ package ca.gc.asc_csa.apogy.core.programs.controllers.ui.parts;
  *     Canadian Space Agency (CSA) - Initial API and implementation
  */
 
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.StyledCellLabelProvider;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
 
-import ca.gc.asc_csa.apogy.core.invocator.ProgramsGroup;
-import ca.gc.asc_csa.apogy.core.invocator.ui.parts.AbstractApogySessionBasedPart;
+import ca.gc.asc_csa.apogy.common.emf.Described;
+import ca.gc.asc_csa.apogy.common.emf.Named;
+import ca.gc.asc_csa.apogy.common.emf.ui.composites.EObjectListComposite;
+import ca.gc.asc_csa.apogy.core.programs.controllers.ControllersConfiguration;
+import ca.gc.asc_csa.apogy.core.programs.controllers.ControllersGroup;
 
-public class ControllerConfigsPart extends AbstractApogySessionBasedPart{
+public class ControllerConfigsPart{
 	
-	protected Composite createContentComposite(Composite parent){
-			return null;	
-		/*return new AdvancedEditorComposite(parent, SWT.None) {
+//@Inject ESelectionService selectionService;
+
+	@PostConstruct
+	public void createPartControl(Composite parent, ESelectionService selectionService) {
+		EObjectListComposite eObjectListComposite = new EObjectListComposite(parent, SWT.None) {
 			@Override
-			protected void newSelection(ISelection selection) 
-			{
-				super.newSelection(selection);
+			protected void newSelection(TreeSelection selection) {
 				selectionService.setSelection(selection);
 			}
-		};*/
+			
+			@Override
+			protected StyledCellLabelProvider getLabelProvider() {
+				return new StyledCellLabelProvider() {
+					@Override
+					public void update(ViewerCell cell) {
+						if (cell.getElement() instanceof ControllersConfiguration) {
+							String text = ((Named) cell.getElement()).getName();
+							if(((ControllersConfiguration) cell.getElement()).isActive()){
+								text += " [Active]";
+							}
+							cell.setText(text);
+						}
+					}
+					
+					@Override
+					public String getToolTipText(Object element) {
+						if (element instanceof ControllersConfiguration) {
+							return ((Described)element).getDescription();
+						}
+						return super.getToolTipText(element);
+					}
+
+					@Override
+					public Point getToolTipShift(Object object) {
+						return new Point(5, 5);
+					}
+
+					@Override
+					public int getToolTipDisplayDelayTime(Object object) {
+						return 500;
+					}
+
+					@Override
+					public int getToolTipTimeDisplayed(Object object) {
+						return 5000;
+					}
+
+				};
+			}
+		};
+
+		if (ControllersGroup.INSTANCE != null) {
+			EList<EObject> controllersGroup = new BasicEList<>();
+			
+			controllersGroup.addAll(ControllersGroup.INSTANCE.getPrograms());
+			eObjectListComposite.setEObjectsList(controllersGroup);
+		}
 	}
-	
-	protected void setSession(ProgramsGroup programsGroup){
-		//((AdvancedEditorComposite)getContentComposite()).setEObject(invocatorSession);
-		
-	}
+
+//	protected Composite createContentComposite(Composite parent) {
+//		return new EObjectListComposite(parent, SWT.None) {
+//			@Override
+//			protected void newSelection(TreeSelection selection) {
+//				selectionService.setSelection(selection);
+//			}
+//			
+//			@Override
+//			protected StyledCellLabelProvider getLabelProvider() {
+//				return new StyledCellLabelProvider() {
+//					@Override
+//					public void update(ViewerCell cell) {
+//						if (cell.getElement() instanceof ControllersConfiguration) {
+//							String text = ((Named) cell.getElement()).getName();
+//							if(((ControllersConfiguration) cell.getElement()).isActive()){
+//								text += " [Active]";
+//							}
+//							cell.setText(text);
+//						}
+//					}
+//					
+//					@Override
+//					public String getToolTipText(Object element) {
+//						if (element instanceof ControllersConfiguration) {
+//							return ((Described)element).getDescription();
+//						}
+//						return super.getToolTipText(element);
+//					}
+//
+//					@Override
+//					public Point getToolTipShift(Object object) {
+//						return new Point(5, 5);
+//					}
+//
+//					@Override
+//					public int getToolTipDisplayDelayTime(Object object) {
+//						return 500;
+//					}
+//
+//					@Override
+//					public int getToolTipTimeDisplayed(Object object) {
+//						return 5000;
+//					}
+//
+//				};
+//			}
+//		};
+//	}
+//	
+//	protected void setSession(InvocatorSession invocatorSession){
+//		EList<EObject> programsGroup = new BasicEList<>();
+//		programsGroup.addAll(ControllersGroup.INSTANCE.getPrograms());
+//		
+//		// TODO
+//		((EObjectListComposite) getContentComposite()).setEObjectsList(programsGroup);
+//		// ((AdvancedEditorComposite)getContentComposite()).setEObject(invocatorSession);
+//		
+//	}
 }
